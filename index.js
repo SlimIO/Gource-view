2cee6c20d2e0dc72f2da8fe4845ee87a5a7c5d4c
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-05-17 14:55:32 +0200

    chore: first release

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Require Node.js Dependencies
-const { appendFile } = require("fs").promises;
 const fs = require("fs");
 const { join } = require("path");
-const { spawn } = require("child_process");
+const { exec, spawn } = require("child_process");
 
 // Require Third-party Dependencies
 require("make-promises-safe");
@@ -17,53 +16,12 @@ const DIR_FILES = join(__dirname, "/logs");
 // Global
 const token = process.env.GITHUB_TOKEN;
 const orgaUrl = `${GITHUB_URL}/${process.env.GITHUB_ORGA}`;
-// const reposUrl = `${GITHUB_URL}/repos/${process.env.GITHUB_ORGA}`;
-const headers = {
-    "User-Agent": "SlimIO",
-    Authorization: `token ${process.env.GITHUB_TOKEN}`,
-    "Retry-After": 5
-};
 
-async function writeCommits(url, repName, sha) {
-    const { data } = await get(`${url}/${sha}`, { headers });
-    // console.log(data);
-    const { commit: { author: { name, date } }, files } = data;
-    let commitToWrite = "";
-    for (const { filename, status } of files) {
-        const timestamp = Date.parse(date);
-        let statusLetter;
-        switch (status) {
-            case "modified": statusLetter = "M"; break;
-            case "removed": statusLetter = "D"; break;
-            case "added": statusLetter = "A"; break;
-            case "renamed": statusLetter = "R"; break;
-            default: throw Error(`Status ${status} for file ${filename} is not repertoried`);
-        }
-        commitToWrite += `${timestamp}|${name}|${statusLetter}|${filename}\n`;
-    }
-    await appendFile(join(DIR_FILES, `./${repName}.txt`), commitToWrite);
-}
 
-async function getRepoCommits(repName) {
-    // console.log(`Repository name: ${repName}`);
-    const commitsUrl = `${reposUrl}/${repName}/commits`;
-    try {
-        const { data: commits } = await get(commitsUrl, { headers });
-        // console.log(`${commitsUrl} validate`);
-        const commitsSha = commits.map((commit) => commit.sha);
-
-        // Retrieve info of commit with sha
-        for (const sha of commitsSha) {
-            await writeCommits(commitsUrl, repName, sha);
-        }
-        console.log(`writeCommits for ${commitsUrl} all writed !`);
-    }
-    catch (err) {
-        console.log(`${commitsUrl} failed !`);
-        console.error(err);
-        console.error(err.stack);
-        throw err;
-    }
+async function getRepoCommitsLogs(repName) {
+    exec(`gource --output-custom-log logs/${repName}.txt clones/${repName}`, async() => {
+        await exec(`sed -i -r "s#(.+)\\|#\\1|/${repName}#" logs/${repName}.txt`);
+    });
 }
 
 async function cloneRep(repName) {
@@ -81,6 +39,7 @@ async function cloneRep(repName) {
             token
         });
         console.log(`${repName} cloned`);
+        await getRepoCommitsLogs(repName);
     }
     catch (err) {
         console.log(`${url} failed`);
@@ -96,28 +55,19 @@ async function getAllRepo() {
 
     const rejects = [];
     console.log("Run all");
-    // try {
     const result = await Promise.all(allRepoName.map((repName) => cloneRep(repName).catch(rejects.push)));
-    // }
-    // catch (err) {
-    //     for (const reject of rejects) {
-    //         console.log(reject);
-    //         console.log();
-    //     }
-    // }
-    // let cmd = "cat ";
-    // const array = [];
-    // for (const repName of allRepoName) {
-    //     array.push(`${repName}.txt`);
-    //     cmd += `${repName}.txt `;
-    // }
-    // array.push("> combined.txt");
-    // cmd += "> combined.txt";
-    // await spawn("cat", [...array], { cwd: DIR_FILES });
+    for (const reject of rejects) {
+        console.log(reject);
+        console.log();
+    }
+
+    await exec("cat logs/*.txt | sort > logs/combined/fullLog.txt", () => {
+        spawn("gource.cmd", ["logs/combined/fullLog.txt", "-s", "0.2"]);
+    });
 }
 
 async function main() {
-    getAllRepo();
+    await getAllRepo();
 }
 main().catch(console.error);
 
